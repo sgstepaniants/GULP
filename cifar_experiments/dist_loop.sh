cc4ca3824e9ec85455afd9b06d4fa82ac788cd48
Hannah Lawrence <hannah.lawrence@yale.edu>
2022-05-24 16:19:02 -0400

    remove old cifar files

--- cifar_experiments/dist_loop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# Run the script
-declare -i batches=250
-
-for (( i = 1; i <= $batches; i++ ))
-do
-    sbatch -n 4 -o distcifar-$i-$batches.log distcifar_script.sh $i $batches
-done
--- cifar_experiments/distcifar_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Loading the required module
-source /etc/profile
-module load anaconda/2022a
-
-# Run the script
-python compute_dists.py $1 $2
